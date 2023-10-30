24e0650c50a0cc6fca7f81c5833aaf2289e65ad0
Simon <simon011130@gmail.com>
2023-10-30 11:01:55 +0000

    update

--- textattack/attack_recipes/swap_label_attack.py
+++ textattack/attack_recipes/swap_label_attack.py
@@ -17,7 +17,7 @@ from textattack.constraints.pre_transformation.instruction_attack import Instruc
 
 from textattack.constraints.semantics.sentence_encoders import UniversalSentenceEncoder
 from textattack.goal_functions import UntargetedClassification
-from textattack.search_methods import GreedyWordSwapWIR
+from textattack.search_methods import GreedyWordSwapWIR, GreedySearch
 from textattack.transformations import (
     CompositeTransformation,
     WordSwapLabel
@@ -35,23 +35,23 @@ class SwapLabel2023(AttackRecipe):
     """
 
     @staticmethod
-    def build(model_wrapper):
+    def build(model_wrapper, verbalizer):
         #
         #  we propose five bug generation methods for TEXTBUGGER:
         #
         transformation = CompositeTransformation(
             [
-                WordSwapLabel()
+                WordSwapLabel(verbalizer=verbalizer)
             ]
         )
 
-        constraints = [RepeatModification(), MaxWordsPerturbed(0.5), InstructionModification(['sentence', 'Example_'])]
+        constraints = [RepeatModification(), MaxWordsPerturbed(max_percent=0.5), InstructionModification(['sentence', 'Example_'])]
         # Goal is untargeted classification
         #
         goal_function = UntargetedClassification(model_wrapper)
         #
         # Greedily swap words with "Word Importance Ranking".
         #
-        search_method = GreedyWordSwapWIR(wir_method="unk", unk_token="<unk>")
+        search_method = GreedySearch()
 
         return Attack(goal_function, constraints, transformation, search_method)
