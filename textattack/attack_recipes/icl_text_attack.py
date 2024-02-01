4e3c51942ed619f468d705b826c0d7c67f35ed12
simonucl <simon011130@gmail.com>
2024-02-01 12:31:04 +0000

    test

--- textattack/attack_recipes/icl_text_attack.py
+++ textattack/attack_recipes/icl_text_attack.py
@@ -78,7 +78,7 @@ class ICLTextAttack(AttackRecipe):
             ]
         )
 
-        constraints = [RepeatModification(), StopwordModification(), InstructionModification(columns_allowed=["Hypothesis_"]), MaxWordsPerturbed(max_percent=0.1)]
+        constraints = [RepeatModification(), StopwordModification(), InstructionModification(columns_allowed=["Hypothesis_", "Premise_"]), MaxWordsPerturbed(max_percent=0.1)]
         # In our experiment, we first use the Universal Sentence
         # Encoder [7], a model trained on a number of natural language
         # prediction tasks that require modeling the meaning of word
